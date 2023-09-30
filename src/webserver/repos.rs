8cd832982374ff70df8c82881a87c6b8b8ed56d9
skcd <sandeepkumarpani888@gmail.com>
2023-09-29 19:25:42 -0700

    [sidecar] expose sync type for the repo

--- src/webserver/repos.rs
+++ src/webserver/repos.rs
@@ -4,7 +4,7 @@
 use axum::{
     extract::{Query, State},
     response::IntoResponse,
-    Extension,
+    Extension, Router,
 };
 use serde::{Deserialize, Serialize};
 
@@ -16,14 +16,14 @@ use crate::{
 use super::types::{json, ApiResponse, Result};
 
 #[derive(Serialize, Debug, Eq)]
-pub(crate) struct Repo {
-    pub(super) provider: Backend,
-    pub(super) name: String,
+pub struct Repo {
+    pub provider: Backend,
+    pub name: String,
     #[serde(rename = "ref")]
-    pub(super) repo_ref: RepoRef,
-    pub(super) local_duplicates: Vec<RepoRef>,
-    pub(super) sync_status: SyncStatus,
-    pub(super) most_common_lang: Option<String>,
+    pub repo_ref: RepoRef,
+    pub local_duplicates: Vec<RepoRef>,
+    pub sync_status: SyncStatus,
+    pub most_common_lang: Option<String>,
 }
 
 impl PartialEq for Repo {
@@ -47,7 +47,7 @@ pub enum QueueState {
 
 #[derive(Serialize)]
 #[serde(rename_all = "snake_case")]
-pub(crate) enum ReposResponse {
+pub enum ReposResponse {
     List(Vec<Repo>),
     Item(Repo),
     SyncQueue(Vec<QueuedRepoStatus>),
@@ -56,14 +56,14 @@ pub(crate) enum ReposResponse {
 }
 
 #[derive(Deserialize)]
-pub(crate) struct RepoParams {
-    pub(crate) repo: RepoRef,
+pub struct RepoParams {
+    pub repo: RepoRef,
 }
 
 impl ApiResponse for ReposResponse {}
 
 /// Synchronize a repo by its id
-pub(super) async fn sync(
+pub async fn sync(
     Query(RepoParams { repo }): Query<RepoParams>,
     State(app): State<Application>,
 ) -> Result<impl IntoResponse> {

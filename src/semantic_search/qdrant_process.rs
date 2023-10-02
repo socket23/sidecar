4c5fa66b73f552f69a5fe6cc3fa1e53770a74318
skcd <sandeepkumarpani888@gmail.com>
2023-10-02 01:50:48 -0700

    [sidecar] start the qdrant server process

--- src/semantic_search/qdrant_process.rs
+++ src/semantic_search/qdrant_process.rs
@@ -1,15 +1,17 @@
 /// We want to keep the qdrant binary running here so we can use that along
 /// with the client to power our semantic search and everything else which is
 /// required.
-use std::process::Child;
+use std::{process::Child, sync::Arc};
 
-#[derive(Default)]
-pub(super) struct QdrantSupervisor {
+use crate::application::config::configuration::Configuration;
+
+pub struct QdrantServerProcess {
     child: Option<Child>,
+    _configuration: Arc<Configuration>,
 }
 
 /// This will drop the child process and when it exits, it will kill the process
-impl Drop for QdrantSupervisor {
+impl Drop for QdrantServerProcess {
     fn drop(&mut self) {
         if let Some(mut child) = self.child.take() {
             child.kill().unwrap();

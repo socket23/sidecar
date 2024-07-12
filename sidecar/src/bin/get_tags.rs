c5603091368380f055d6ffa8543fccfee2f661d7
sartoshi-foot-dao <99770068+sartoshi-foot-dao@users.noreply.github.com>
2024-07-12 14:30:05 +0100

    add python test file

--- sidecar/src/bin/get_tags.rs
+++ sidecar/src/bin/get_tags.rs
@@ -1,3 +1,24 @@
+use sidecar::chunking::languages::TSLanguageParsing;
+
 fn main() {
-    println!("Hello, world!");
+    let ts_language_parsing = TSLanguageParsing::init();
+    let source_code = r#"
+    fn main() {
+        println!("Hello, world!");
+    }
+    "#;
+
+    if let Some(_) = ts_language_parsing.for_lang("python") {
+        let chunks = ts_language_parsing.chunk_file(
+            "example.rs",
+            source_code,
+            Some("rs"),
+            Some("rust"),
+        );
+        for chunk in chunks {
+            println!("{:?}", chunk);
+        }
+    } else {
+        println!("Language configuration not found for 'rust'");
+    }
 }

010aa5002126aa60dd35c3e6c08e813c7cd1bcdb
sartoshi-foot-dao <99770068+sartoshi-foot-dao@users.noreply.github.com>
2024-07-14 23:36:17 +0100

    update binary path

--- sidecar/src/bin/try_repomap.rs
+++ sidecar/src/bin/try_repomap.rs
@@ -5,7 +5,15 @@ fn main() {
 
     let ts_parsing = Arc::new(TSLanguageParsing::init());
 
-    let fname_path = PathBuf::from("src/repomap/types.rs");
+    let fname_path = PathBuf::from("sidecar/src/repomap/types.rs");
 
-    repomap.get_ranked_tags(&fname_path, ts_parsing);
+    let project_root = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .unwrap()
+        .to_path_buf();
+    let full_path = project_root.join(&fname_path);
+
+    println!("{:?}", full_path);
+
+    repomap.get_ranked_tags(&[full_path], &[fname_path], ts_parsing);
 }

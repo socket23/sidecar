a16d2f9ba966870d51d7827b82082955c265f70e
sartoshi-foot-dao <99770068+sartoshi-foot-dao@users.noreply.github.com>
2024-07-15 10:08:24 +0100

    handle multiple fnames

--- sidecar/src/bin/try_repomap.rs
+++ sidecar/src/bin/try_repomap.rs
@@ -1,18 +1,23 @@
 use sidecar::{chunking::languages::TSLanguageParsing, repomap::types::RepoMap};
-use std::{path::PathBuf, sync::Arc};
+use std::{fs::read_dir, path::PathBuf, sync::Arc};
 fn main() {
     let repomap = RepoMap::new(PathBuf::new());
 
     let ts_parsing = Arc::new(TSLanguageParsing::init());
 
-    let file_names = vec!["src/repomap/types.rs"];
+    let file_names = match (read_dir("src/repomap")) {
+        Ok(read_dir) => {
+            dbg!(read_dir);
+        }
+        Err(e) => eprintln!("Error {:?}", e),
+    };
 
-    let paths: Vec<PathBuf> = file_names
-        .iter()
-        .map(|fname| FullPath::new(fname).path)
-        .collect();
+    // let paths: Vec<PathBuf> = file_names
+    //     .iter()
+    //     .map(|fname| FullPath::new(fname).path)
+    //     .collect();
 
-    repomap.get_ranked_tags(&paths, &paths, ts_parsing);
+    // repomap.get_ranked_tags(&paths, &paths, ts_parsing);
 }
 
 struct FullPath {

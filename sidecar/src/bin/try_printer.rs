370a67e7142792fc3f8021cb8c9a3cdb3dcaae45
sartoshi-foot-dao <99770068+sartoshi-foot-dao@users.noreply.github.com>
2024-07-21 16:42:29 +0100

    [repomap] relieve struct from ownership of the tree it parses and prints

--- sidecar/src/bin/try_printer.rs
+++ sidecar/src/bin/try_printer.rs
@@ -1,12 +1,22 @@
-use sidecar::{chunking::languages::TSLanguageParsing, repomap::tree_printer::TreeContext};
+use sidecar::{chunking::languages::TSLanguageParsing, repomap::tree_printer::TreePrinter};
 
 fn main() {
-    let printer = TreeContext::new(
-        "test.ts".to_string(),
-        "let value: string | undefined | null = null;
+    let filenames = vec!["test.ts".to_string()];
+
+    let test_code = "let value: string | undefined | null = null;
         value = 'hello';
         value = undefined;"
-            .to_string(),
-        &TSLanguageParsing::init(),
-    );
+        .to_string();
+
+    let ts_parsing = TSLanguageParsing::init();
+
+    let config = ts_parsing.for_file_path("test.ts").unwrap();
+
+    let tree = config.get_tree_sitter_tree(test_code.as_bytes()).unwrap();
+
+    let mut printer = TreePrinter::new(&tree, test_code).unwrap();
+
+    let mut cursor = tree.walk();
+
+    printer.walk_tree(&mut cursor);
 }
--- sidecar/src/repomap/tree_printer.rs
+++ sidecar/src/repomap/tree_printer.rs
@@ -1,5 +1,5 @@
 use crate::chunking::languages::TSLanguageParsing;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use thiserror::Error;
 use tree_sitter::{Node, Tree, TreeCursor};
 
@@ -15,8 +15,8 @@ pub enum TreePrinterError {
     IoError(#[from] std::io::Error),
 }
 
-pub struct TreeContext<'a> {
-    filename: String,
+pub struct TreePrinter<'a> {
+    tree: &'a Tree,
     code: String,
     line_number: bool,
     parent_context: bool,
@@ -27,47 +27,19 @@ pub struct TreeContext<'a> {
     header_max: usize,
     show_top_of_file_parent_scope: bool,
     loi_pad: usize,
-    scopes: Vec<HashSet<usize>>,
-    header: Vec<Vec<(usize, usize, usize)>>,
-    nodes: Vec<Vec<&'a Node<'a>>>, // tree-sitter node requires lifetime parameter
+    scopes: Vec<HashSet<usize>>, // the starting lines of the nodes that span the line
+    header: Vec<Vec<(usize, usize, usize)>>, // the size, start line, end line of the nodes that span the line
+    nodes: Vec<Vec<Node<'a>>>,               // tree-sitter node requires lifetime parameter
     num_lines: usize,
-    tree: Tree,
     output: Vec<String>,
 }
 
-impl<'a> TreeContext<'a> {
-    pub fn new(
-        filename: String,
-        code: String,
-        ts_parser: &TSLanguageParsing,
-    ) -> Result<Self, TreePrinterError> {
-        let ts_config = ts_parser
-            .for_file_path(&filename)
-            .ok_or(TreePrinterError::MissingConfig(filename.clone()))?;
-
-        let tree = ts_config
-            .get_tree_sitter_tree(code.as_bytes())
-            .ok_or(TreePrinterError::ParseError(filename.clone()))?;
-
+impl<'a> TreePrinter<'a> {
+    pub fn new(tree: &'a Tree, code: String) -> Result<Self, TreePrinterError> {
         let num_lines = code.lines().count();
 
-        let root_node = &tree.root_node();
-
-        // iterate through nodes
-        // for each node, get start and end positions
-        // add to nodes
-
-        let mut cursor = root_node.walk();
-
-        TreeContext::walk_tree(&mut cursor);
-
-        // // Traverse child nodes
-        // for child in root_node.children(&mut cursor) {
-        //     TreeContext::walk_tree(child);
-        // }
-
         Ok(Self {
-            filename,
+            tree,
             code,
             line_number: false,
             parent_context: true,
@@ -82,7 +54,6 @@ impl<'a> TreeContext<'a> {
             header: vec![Vec::new(); num_lines],
             nodes: vec![Vec::new(); num_lines],
             num_lines,
-            tree: tree.clone(),
             output: vec![],
         })
     }
@@ -92,46 +63,35 @@ impl<'a> TreeContext<'a> {
         println!("Node field_name: {:?}", cursor.field_name());
     }
 
-    fn walk_tree(cursor: &mut TreeCursor) {
-        // Create a cursor for traversing child nodes
-        println!("Starting node:");
-        TreeContext::print_node_at_cursor(&cursor);
+    /// TreeCursor shares lifetime with self.nodes
+    pub fn walk_tree(&mut self, cursor: &mut TreeCursor<'a>) {
+        let node = cursor.node();
 
-        if !cursor.goto_first_child() {
-            println!("No first child");
-            return;
+        let start_line = node.start_position().row;
+        let end_line = node.end_position().row;
+        let size = end_line - start_line;
+
+        self.nodes[start_line].push(node);
+
+        if size > 0 {
+            self.header[start_line].push((size, start_line, end_line));
         }
 
-        println!("First child");
-        TreeContext::print_node_at_cursor(&cursor);
-
-        let mut sibling_index = 0;
-        while cursor.goto_next_sibling() {
-            println!("sibling {sibling_index}");
-            TreeContext::print_node_at_cursor(&cursor);
-            sibling_index += 1;
+        // for every line the node spans, add the position of its start line
+        for i in start_line..=end_line {
+            self.scopes[i].insert(start_line);
         }
 
-        println!("No next sibling");
+        if cursor.goto_first_child() {
+            loop {
+                self.walk_tree(cursor);
+                if !cursor.goto_next_sibling() {
+                    break;
+                }
+            }
+            cursor.goto_parent();
+        }
     }
-
-    // fn walk_tree(&mut self, node: Node<'a>) {
-    //     let start = node.start_position();
-    //     let end = node.end_position();
-
-    //     let start_line = start.row;
-    //     let start_col = start.column;
-    //     let end_line = end.row;
-    //     let end_col = end.column;
-
-    //     let size = end_line - start_line;
-    //     self.nodes[start_line].push(&node);
-    // }
-
-    // pub fn print_tree(&'a mut self) {
-    //     let root_node = self.tree.root_node();
-    //     self.walk_tree(root_node);
-    // }
 
     // walk tree
 
@@ -140,16 +100,4 @@ impl<'a> TreeContext<'a> {
     // add context()
 
     // format
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_tree_printer() -> Result<(), TreePrinterError> {
-        let ts_parser = TSLanguageParsing::init();
-        let tree_context = TreeContext::new("test.ts".to_string(), "".to_string(), &ts_parser)?;
-        Ok(())
-    }
 }

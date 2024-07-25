db7f4817077ff58f287e5a1778ff8d38ceb48a44
sartoshi-foot-dao <99770068+sartoshi-foot-dao@users.noreply.github.com>
2024-07-25 17:08:03 +0100

    [repomap] add debugging comments

--- sidecar/src/bin/try_repomap.rs
+++ sidecar/src/bin/try_repomap.rs
@@ -7,7 +7,7 @@ fn main() {
     let repomap = RepoMap::new(fs).with_map_tokens(1000);
 
     // change this to the directory you want to generate a repomap for
-    let dir = PathBuf::from("src/repomap");
+    let dir = PathBuf::from(".");
     let project_root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
 
     let full_path = project_root.join(&dir);

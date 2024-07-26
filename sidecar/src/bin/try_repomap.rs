da8439b8d185d9eca038c76b045c39187538abbe
skcd <sandeepkumarpani888@gmail.com>
2024-07-26 02:58:28 +0100

    [sidecar] fix crashes on whole sidecar

--- sidecar/src/bin/try_repomap.rs
+++ sidecar/src/bin/try_repomap.rs
@@ -1,15 +1,17 @@
 use sidecar::repomap::types::RepoMap;
-use std::path::PathBuf;
+use std::path::Path;
 
 #[tokio::main]
 async fn main() {
     let repomap = RepoMap::new().with_map_tokens(1000);
 
     // change this to the directory you want to generate a repomap for
-    let dir = PathBuf::from(".");
-    let project_root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    // let dir = PathBuf::from(".");
+    // let project_root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
 
-    let full_path = project_root.join(&dir);
+    // let full_path = project_root.join(&dir);
+
+    let full_path = Path::new("/Users/skcd/scratch/sidecar");
 
     let repomap = repomap.get_repo_map(&full_path).await.unwrap();
 
--- sidecar/src/repomap/file/git.rs
+++ sidecar/src/repomap/file/git.rs
@@ -1,4 +1,11 @@
-use std::{collections::HashMap, path::Path};
+use std::{
+    collections::{BTreeSet, HashMap},
+    path::Path,
+};
+
+use once_cell::sync::Lazy;
+use regex::Regex;
+use smallvec::SmallVec;
 
 use super::errors::FileError;
 
@@ -29,66 +36,88 @@ impl GitWalker {
         }
 
         let git = git.expect("if let Err to hold");
-        let cloned_git = git.clone();
-
         let local_git = git.to_thread_local();
-        let tree = local_git
-            .head()
-            .map_err(|_e| FileError::GixError)?
-            .peel_to_commit_in_place()
-            .map_err(|_e| FileError::GixError)?
-            .tree()
-            .map_err(|_e| FileError::GixError)?;
-        let trees = vec![(true, "HEAD".to_owned(), tree)];
+        let mut head = local_git.head().expect("get this");
+        let trees = vec![(
+            true,
+            "HEAD".to_owned(),
+            head.peel_to_commit_in_place()
+                .expect("to work")
+                .tree()
+                .expect("to work"),
+        )];
 
         let directory_ref: &Path = directory.as_ref();
 
-        Ok(trees
+        let entries = trees
             .into_iter()
-            .flat_map(|(is_head, _, tree)| {
+            .flat_map(|(is_head, branch, tree)| {
                 let files = tree.traverse().breadthfirst.files().unwrap().into_iter();
 
                 files
-                    .map(|file| (file, cloned_git.clone()))
-                    .map(move |(entry, git)| {
+                    .map(move |entry| {
                         let strpath = String::from_utf8_lossy(entry.filepath.as_ref());
                         let full_path = directory_ref.join(strpath.as_ref());
                         (
                             is_head,
+                            branch.clone(),
                             full_path.to_string_lossy().to_string(),
                             entry.mode,
                             entry.oid,
-                            git,
                         )
                     })
+                    .filter(|(_, _, path, _, _)| should_index(path))
             })
-            .map(|(_, file, mode, oid, git)| {
-                let kind = if mode.is_tree() {
-                    FileType::Directory
-                } else if mode.is_blob() {
-                    FileType::File
-                } else {
-                    FileType::NotTracked
-                };
+            .fold(
+                HashMap::new(),
+                |mut acc, (is_head, branch, file, mode, oid)| {
+                    let kind = if mode.is_tree() {
+                        FileType::Directory
+                    } else if mode.is_blob() {
+                        FileType::File
+                    } else {
+                        FileType::NotTracked
+                    };
 
-                let git = git.to_thread_local();
-                let Ok(Some(_)) = git.try_find_object(oid) else {
-                    return None;
-                };
-
-                match kind {
-                    FileType::File => {
-                        let file_content = std::fs::read(file.to_owned());
-                        match file_content {
-                            Ok(file_content) => Some((file, file_content)),
-                            Err(_) => None,
-                        }
+                    let branches = acc.entry((file, kind, oid)).or_insert_with(BTreeSet::new);
+                    if is_head {
+                        branches.insert("HEAD".to_string());
                     }
-                    _ => None,
-                }
-            })
-            .filter_map(|s| s)
-            .collect::<HashMap<String, Vec<u8>>>())
+
+                    branches.insert(branch);
+                    acc
+                },
+            );
+
+        // Now we want to read the content of all the files in parallel
+        {
+            let start_time = std::time::Instant::now();
+            use rayon::prelude::*;
+            let values = entries
+                .into_par_iter()
+                .filter_map(|((path, kind, oid), _)| {
+                    let git = git.to_thread_local();
+                    let Ok(Some(_)) = git.try_find_object(oid) else {
+                        return None;
+                    };
+
+                    let entry = match kind {
+                        FileType::File => {
+                            println!("git_walking::path::({})", path);
+                            match std::fs::read(path.to_owned()) {
+                                Ok(content) => Some((path, content)),
+                                Err(_) => None,
+                            }
+                        }
+                        FileType::Directory => None,
+                        FileType::NotTracked => None,
+                    };
+                    entry
+                })
+                .collect::<HashMap<_, _>>();
+            println!("Time taken: {}", start_time.elapsed().as_micros());
+            Ok(values)
+        }
     }
 
     fn get_files_recursive(
@@ -111,5 +140,106 @@ impl GitWalker {
             }
         }
         Ok(())
+    }
+}
+
+fn should_index<P: AsRef<Path>>(p: &P) -> bool {
+    let path = p.as_ref();
+
+    if path.components().any(|c| c.as_os_str() == ".git") {
+        return false;
+    }
+
+    #[rustfmt::skip]
+    const EXT_BLACKLIST: &[&str] = &[
+        // graphics
+        "png", "jpg", "jpeg", "ico", "bmp", "bpg", "eps", "pcx", "ppm", "tga", "tiff", "wmf", "xpm",
+        "svg", "riv",
+        // fonts
+        "ttf", "woff2", "fnt", "fon", "otf",
+        // documents
+        "pdf", "ps", "doc", "dot", "docx", "dotx", "xls", "xlsx", "xlt", "odt", "ott", "ods", "ots", "dvi", "pcl",
+        // media
+        "mp3", "ogg", "ac3", "aac", "mod", "mp4", "mkv", "avi", "m4v", "mov", "flv",
+        // compiled
+        "jar", "pyc", "war", "ear",
+        // compression
+        "tar", "gz", "bz2", "xz", "7z", "bin", "apk", "deb", "rpm",
+        // executable
+        "com", "exe", "out", "coff", "obj", "dll", "app", "class",
+        // misc.
+        "log", "wad", "bsp", "bak", "sav", "dat", "lock",
+        // dylib
+        "dylib",
+        // onnx file
+        "onnx",
+        // .so files
+        "so",
+    ];
+
+    let Some(ext) = path.extension() else {
+        // if we have no extension, then do not index
+        // this gets rid of binary blobs
+        return false;
+    };
+
+    let ext = ext.to_string_lossy();
+    if EXT_BLACKLIST.contains(&&*ext) {
+        return false;
+    }
+
+    static VENDOR_PATTERNS: Lazy<HashMap<&'static str, SmallVec<[Regex; 1]>>> = Lazy::new(|| {
+        let patterns: &[(&[&str], &[&str])] = &[
+            (
+                &["go", "proto"],
+                &["^(vendor|third_party)/.*\\.\\w+$", "\\w+\\.pb\\.go$"],
+            ),
+            (
+                &["js", "jsx", "ts", "tsx", "css", "md", "json", "txt", "conf"],
+                &["^(node_modules|vendor|dist)/.*\\.\\w+$"],
+            ),
+        ];
+
+        patterns
+            .iter()
+            .flat_map(|(exts, rxs)| exts.iter().map(move |&e| (e, rxs)))
+            .map(|(ext, rxs)| {
+                let regexes = rxs
+                    .iter()
+                    .filter_map(|source| match Regex::new(source) {
+                        Ok(r) => Some(r),
+                        Err(_) => None,
+                    })
+                    .collect();
+
+                (ext, regexes)
+            })
+            .collect()
+    });
+
+    match VENDOR_PATTERNS.get(&*ext) {
+        None => true,
+        Some(rxs) => !rxs.iter().any(|r| r.is_match(&path.to_string_lossy())),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    use super::should_index;
+
+    #[test]
+    fn test_parsing_bin_file() {
+        let path = Path::new("/Users/skcd/scratch/sidecar/sidecar/qdrant/qdrant_mac");
+        assert!(!should_index(&path));
+    }
+
+    #[test]
+    fn test_onxx_file() {
+        let path = Path::new(
+            "/Users/skcd/scratch/sidecar/sidecar/models/all-MiniLM-L6-v2/onnx/model.onnx",
+        );
+        assert!(!should_index(&path));
     }
 }

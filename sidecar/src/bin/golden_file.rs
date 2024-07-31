22a18435ab6fd691250172751dbd38fb35b47d9d
sartoshi-foot-dao <99770068+sartoshi-foot-dao@users.noreply.github.com>
2024-07-31 10:44:45 +0100

    add django test

--- sidecar/src/bin/golden_file.rs
+++ sidecar/src/bin/golden_file.rs
@@ -164,6 +164,8 @@ async fn test_golden_file_search(task: &Task, root_dir: &str) {
                         );
                         println!("Symbols: {:?}", symbols);
                         println!("Golden file: {}", task.golden_file);
+
+                        println!("===========================================\nRequest ID: {}\nParea AI: {}\n===========================================", request_id.to_string(), parea_url);
                         break
                     }
                     Err(e) => {
@@ -595,11 +597,26 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_django_sqlmigrate() {
+    async fn test_django_sqlmigrate_10087() {
         let task = Task::new(
             "core/management/commands/sqlmigrate.py".to_string(),
             r#"Misleading sqlmigrate "App 'apps.somethings' does not have migrations." error message Description This ticket is very similar to https://code.djangoproject.com/ticket/29506 As shown above, validation should be added sqlmigrate."#.to_string(),
         );
+        test_golden_file_search(&task, DJANGO_ROOT_DIR).await;
+    }
+
+    #[tokio::test]
+    // commit: e7fd69d051eaa67cb17f172a39b57253e9cb831a
+    async fn test_django_10914() {
+        let task = Task::new("django/conf/global_settings.py".to_string(), r#""Set default FILE_UPLOAD_PERMISSION to 0o644.
+        Description
+            
+        Hello,
+        As far as I can see, the ​File Uploads documentation page does not mention any permission issues.
+        What I would like to see is a warning that in absence of explicitly configured FILE_UPLOAD_PERMISSIONS, the permissions for a file uploaded to FileSystemStorage might not be consistent depending on whether a MemoryUploadedFile or a TemporaryUploadedFile was used for temporary storage of the uploaded data (which, with the default FILE_UPLOAD_HANDLERS, in turn depends on the uploaded data size).
+        The tempfile.NamedTemporaryFile + os.rename sequence causes the resulting file permissions to be 0o0600 on some systems (I experience it here on CentOS 7.4.1708 and Python 3.6.5). In all probability, the implementation of Python's built-in tempfile module explicitly sets such permissions for temporary files due to security considerations.
+        I found mentions of this issue ​on GitHub, but did not manage to find any existing bug report in Django's bug tracker.
+        ""#.to_string());
         test_golden_file_search(&task, DJANGO_ROOT_DIR).await;
     }
 }

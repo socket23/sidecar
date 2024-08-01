f364d6e81968339d636fc54dcf9f701604a559ee
sartoshi-foot-dao <99770068+sartoshi-foot-dao@users.noreply.github.com>
2024-08-01 11:25:45 +0100

    [search] update script

--- sidecar/src/bin/try_important_files_finder.rs
+++ sidecar/src/bin/try_important_files_finder.rs
@@ -1,8 +1,51 @@
+use std::sync::Arc;
+
 use llm_client::clients::types::LLMType;
-use sidecar::agentic::tool::code_symbol::tree::ImportantFilesFinderBroker;
+
+use sidecar::agentic::tool::{broker::ToolBroker, code_symbol::tree::ImportantFilesFinderBroker};
 
 fn main() {
     let llm = LLMType::GeminiProFlash;
     let tree = "some tree";
-    let broker = ImportantFilesFinderBroker::new(tree.to_string(), llm);
+
+    let request_id = uuid::Uuid::new_v4();
+    let request_id_str = request_id.to_string();
+    let parea_url = format!(
+        r#"https://app.parea.ai/logs?colViz=%7B%220%22%3Afalse%2C%221%22%3Afalse%2C%222%22%3Afalse%2C%223%22%3Afalse%2C%22error%22%3Afalse%2C%22deployment_id%22%3Afalse%2C%22feedback_score%22%3Afalse%2C%22time_to_first_token%22%3Afalse%2C%22scores%22%3Afalse%2C%22start_timestamp%22%3Afalse%2C%22user%22%3Afalse%2C%22session_id%22%3Afalse%2C%22target%22%3Afalse%2C%22experiment_uuid%22%3Afalse%2C%22dataset_references%22%3Afalse%2C%22in_dataset%22%3Afalse%2C%22event_type%22%3Afalse%2C%22request_type%22%3Afalse%2C%22evaluation_metric_names%22%3Afalse%2C%22request%22%3Afalse%2C%22calling_node%22%3Afalse%2C%22edges%22%3Afalse%2C%22metadata_evaluation_metric_names%22%3Afalse%2C%22metadata_event_type%22%3Afalse%2C%22metadata_0%22%3Afalse%2C%22metadata_calling_node%22%3Afalse%2C%22metadata_edges%22%3Afalse%2C%22metadata_root_id%22%3Afalse%7D&filter=%7B%22filter_field%22%3A%22meta_data%22%2C%22filter_operator%22%3A%22equals%22%2C%22filter_key%22%3A%22root_id%22%2C%22filter_value%22%3A%22{request_id_str}%22%7D&page=1&page_size=50&time_filter=1m"#
+    );
+    println!("===========================================\nRequest ID: {}\nParea AI: {}\n===========================================", request_id.to_string(), parea_url);
+    let editor_url = "http://localhost:42424".to_owned();
+    let anthropic_api_keys = LLMProviderAPIKeys::Anthropic(AnthropicAPIKey::new("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".to_owned()));
+    let anthropic_llm_properties = LLMProperties::new(
+        LLMType::ClaudeSonnet,
+        LLMProvider::Anthropic,
+        anthropic_api_keys.clone(),
+    );
+    let llama_70b_properties = LLMProperties::new(
+        LLMType::Llama3_1_70bInstruct,
+        LLMProvider::FireworksAI,
+        LLMProviderAPIKeys::FireworksAI(FireworksAPIKey::new(
+            "s8Y7yIXdL0lMeHHgvbZXS77oGtBAHAsfsLviL2AKnzuGpg1n".to_owned(),
+        )),
+    );
+    let editor_parsing = Arc::new(EditorParsing::default());
+    let symbol_broker = Arc::new(SymbolTrackerInline::new(editor_parsing.clone()));
+    let tool_broker = Arc::new(ToolBroker::new(
+        Arc::new(
+            LLMBroker::new(LLMBrokerConfiguration::new(default_index_dir()))
+                .await
+                .expect("to initialize properly"),
+        ),
+        Arc::new(CodeEditBroker::new()),
+        symbol_broker.clone(),
+        Arc::new(TSLanguageParsing::init()),
+        ToolBrokerConfiguration::new(None, true),
+        LLMProperties::new(
+            LLMType::Gpt4O,
+            LLMProvider::OpenAI,
+            LLMProviderAPIKeys::OpenAI(OpenAIProvider::new(
+                "sk-proj-BLaSMsWvoO6FyNwo9syqT3BlbkFJo3yqCyKAxWXLm4AvePtt".to_owned(),
+            )),
+        ),
+    ));
 }

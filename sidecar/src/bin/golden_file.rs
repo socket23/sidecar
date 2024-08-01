97eab27cf405f652f4c3876d145a577608442dd4
sartoshi-foot-dao <99770068+sartoshi-foot-dao@users.noreply.github.com>
2024-08-01 12:27:49 +0100

    [search] add basic testing loop

--- sidecar/src/bin/golden_file.rs
+++ sidecar/src/bin/golden_file.rs
@@ -19,6 +19,8 @@ use sidecar::{
         tool::{
             broker::{ToolBroker, ToolBrokerConfiguration},
             code_edit::models::broker::CodeEditBroker,
+            code_symbol::tree::ImportantFilesFinderQuery,
+            input::ToolInput,
         },
     },
     chunking::{editor_parsing::EditorParsing, languages::TSLanguageParsing},
@@ -50,10 +52,21 @@ fn default_index_dir() -> PathBuf {
 
 #[tokio::main]
 async fn main() {
-    let csv_path = "/Users/zi/codestory/sidecar/sidecar/src/bin/swe_lite_formula.csv";
-    let repo = "sqlfluff/sqlfluff";
+    let _ = test_important_file_finder().await;
+}
 
-    let problems = read_problems_from_csv(csv_path, repo);
+async fn test_important_file_finder() {
+    let tree = "some tree";
+
+    let llm_type = LLMType::GeminiProFlash;
+
+    let finder_query = ImportantFilesFinderQuery::new(tree.to_string(), llm_type);
+
+    let tool_input = ToolInput::ImportantFilesFinder(finder_query);
+
+    let symbol_manager = make_symbol_manager().await;
+
+    let _ = symbol_manager.test_important_file_finder(tool_input).await;
 }
 
 fn read_problems_from_csv(path: &str, repo: &str) -> Result<Vec<Task>, Box<dyn Error>> {
@@ -62,6 +75,66 @@ fn read_problems_from_csv(path: &str, repo: &str) -> Result<Vec<Task>, Box<dyn E
     let mut problems = Vec::new();
 
     Ok(problems)
+}
+
+async fn make_symbol_manager() -> SymbolManager {
+    let request_id = uuid::Uuid::new_v4();
+    let request_id_str = request_id.to_string();
+    let parea_url = format!(
+        r#"https://app.parea.ai/logs?colViz=%7B%220%22%3Afalse%2C%221%22%3Afalse%2C%222%22%3Afalse%2C%223%22%3Afalse%2C%22error%22%3Afalse%2C%22deployment_id%22%3Afalse%2C%22feedback_score%22%3Afalse%2C%22time_to_first_token%22%3Afalse%2C%22scores%22%3Afalse%2C%22start_timestamp%22%3Afalse%2C%22user%22%3Afalse%2C%22session_id%22%3Afalse%2C%22target%22%3Afalse%2C%22experiment_uuid%22%3Afalse%2C%22dataset_references%22%3Afalse%2C%22in_dataset%22%3Afalse%2C%22event_type%22%3Afalse%2C%22request_type%22%3Afalse%2C%22evaluation_metric_names%22%3Afalse%2C%22request%22%3Afalse%2C%22calling_node%22%3Afalse%2C%22edges%22%3Afalse%2C%22metadata_evaluation_metric_names%22%3Afalse%2C%22metadata_event_type%22%3Afalse%2C%22metadata_0%22%3Afalse%2C%22metadata_calling_node%22%3Afalse%2C%22metadata_edges%22%3Afalse%2C%22metadata_root_id%22%3Afalse%7D&filter=%7B%22filter_field%22%3A%22meta_data%22%2C%22filter_operator%22%3A%22equals%22%2C%22filter_key%22%3A%22root_id%22%2C%22filter_value%22%3A%22{request_id_str}%22%7D&page=1&page_size=50&time_filter=1m"#
+    );
+    println!("===========================================\nRequest ID: {}\nParea AI: {}\n===========================================", request_id.to_string(), parea_url);
+    let editor_url = "http://localhost:42424".to_owned();
+    let anthropic_api_keys = LLMProviderAPIKeys::Anthropic(AnthropicAPIKey::new("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".to_owned()));
+    let anthropic_llm_properties = LLMProperties::new(
+        LLMType::ClaudeSonnet,
+        LLMProvider::Anthropic,
+        anthropic_api_keys.clone(),
+    );
+    let llama_70b_properties = LLMProperties::new(
+        LLMType::Llama3_1_70bInstruct,
+        LLMProvider::FireworksAI,
+        LLMProviderAPIKeys::FireworksAI(FireworksAPIKey::new(
+            "s8Y7yIXdL0lMeHHgvbZXS77oGtBAHAsfsLviL2AKnzuGpg1n".to_owned(),
+        )),
+    );
+    let editor_parsing = Arc::new(EditorParsing::default());
+    let symbol_broker = Arc::new(SymbolTrackerInline::new(editor_parsing.clone()));
+    let tool_broker = Arc::new(ToolBroker::new(
+        Arc::new(
+            LLMBroker::new(LLMBrokerConfiguration::new(default_index_dir()))
+                .await
+                .expect("to initialize properly"),
+        ),
+        Arc::new(CodeEditBroker::new()),
+        symbol_broker.clone(),
+        Arc::new(TSLanguageParsing::init()),
+        ToolBrokerConfiguration::new(None, true),
+        LLMProperties::new(
+            LLMType::Gpt4O,
+            LLMProvider::OpenAI,
+            LLMProviderAPIKeys::OpenAI(OpenAIProvider::new(
+                "sk-proj-BLaSMsWvoO6FyNwo9syqT3BlbkFJo3yqCyKAxWXLm4AvePtt".to_owned(),
+            )),
+        ),
+    ));
+
+    let user_context = UserContext::new(vec![], vec![], None, vec![]);
+
+    let (sender, mut receiver) = tokio::sync::mpsc::unbounded_channel();
+
+    let symbol_manager = SymbolManager::new(
+        tool_broker.clone(),
+        symbol_broker.clone(),
+        editor_parsing,
+        editor_url.to_owned(),
+        sender,
+        anthropic_llm_properties.clone(),
+        user_context.clone(),
+        request_id.to_string(),
+    );
+
+    symbol_manager
 }
 
 /// Copied from code_editing_flow binary
@@ -121,7 +194,6 @@ async fn test_golden_file_search(task: &Task, root_dir: &str) {
         user_context.clone(),
         request_id.to_string(),
     );
-
     let initial_request = SymbolInputEvent::new(
         user_context,
         LLMType::ClaudeSonnet,

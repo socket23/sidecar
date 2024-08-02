11c793d33a69e744c88480eb57d028135e00ff35
sartoshi-foot-dao <99770068+sartoshi-foot-dao@users.noreply.github.com>
2024-08-02 10:56:15 +0100

    [search] limit symbols returned

--- sidecar/src/agentic/tool/code_symbol/models/anthropic.rs
+++ sidecar/src/agentic/tool/code_symbol/models/anthropic.rs
@@ -5283,7 +5283,8 @@ We have to add the newly created endpoint in inline_completion to add support fo
 </user_query>
 
 Remember your reply should always be contained in <reply> tags and follow the format which we have shown you before in the system message.
-Do not forget to include the <file_path> in your reply"#
+Do not forget to include the <file_path> in your reply
+Return no more than 20 symbols"#
         )
     }
 }

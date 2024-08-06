b7a6449c1517e38a1f5ddeb636e4315f7f42f2d2
sartoshi-foot-dao <99770068+sartoshi-foot-dao@users.noreply.github.com>
2024-08-06 13:00:53 +0100

    [agentic-search] implement trait for google_studio

--- sidecar/src/agentic/tool/search/google_studio.rs
+++ sidecar/src/agentic/tool/search/google_studio.rs
@@ -1,1 +1,16 @@
-struct PlanGenerator {}
+use async_trait::async_trait;
+
+use super::agentic::{GenerateSearchPlan, GenerateSearchPlanError, SearchPlanContext};
+
+struct GoogleStudioPlanGenerator {}
+
+#[async_trait]
+impl GenerateSearchPlan for GoogleStudioPlanGenerator {
+    async fn generate_search_plan(
+        &self,
+        query: &str,
+        context: SearchPlanContext,
+    ) -> Result<String, GenerateSearchPlanError> {
+        todo!();
+    }
+}

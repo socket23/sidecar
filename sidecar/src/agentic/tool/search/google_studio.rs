afde1aeb7f449d0af7532950106b71c3bb8f648b
sartoshi-foot-dao <99770068+sartoshi-foot-dao@users.noreply.github.com>
2024-08-09 11:32:16 +0100

    [agentic] add user and system messages

--- sidecar/src/agentic/tool/search/google_studio.rs
+++ sidecar/src/agentic/tool/search/google_studio.rs
@@ -1,4 +1,5 @@
 use async_trait::async_trait;
+use gix::attrs::Search;
 use llm_client::{
     broker::LLMBroker,
     clients::types::{LLMClientCompletionRequest, LLMClientMessage},
@@ -7,8 +8,9 @@ use std::{sync::Arc, time::Instant};
 
 use crate::agentic::{symbol::identifier::LLMProperties, tool::search::agentic::SearchPlanContext};
 
-use super::agentic::{
-    GenerateSearchPlan, GenerateSearchPlanError, SearchPlanQuery, SearchPlanResponse,
+use super::{
+    agentic::{GenerateSearchPlan, GenerateSearchPlanError, SearchPlanQuery, SearchPlanResponse},
+    exp::{Context, SearchQuery},
 };
 
 pub struct GoogleStudioPlanGenerator {
@@ -66,6 +68,60 @@ Context:
             request.user_query(),
             context,
         )
+    }
+
+    pub fn system_message_for_generate_search_plan(&self, context: &Context) -> String {
+        format!(
+            r#"You are an autonomous AI assistant.
+Your task is to locate the code relevant to an issue.
+
+# Instructions:
+
+1. Understand The Issue:
+Read the <issue> tag to understand the issue.
+
+2. Review Current File Context:
+Examine the <file_context> tag to see which files and code spans have already been identified.
+If you believe that all relevant files have been identified, you can finish the search by setting complete to true.
+
+3. Consider the Necessary Search Parameters:
+Determine if specific file types, directories, function or class names or code patterns are mentioned in the issue.
+If you can you should always try to specify the search parameters as accurately as possible.
+You can do more than one search request at the same time so you can try different search parameters to cover all possible relevant code.
+
+4. Ensure At Least One Search Parameter:
+Make sure that at least one of query, code_snippet, class_name, or function_name is provided.
+
+5. Formulate the Search function:
+Set at least one of the search paramaters `query`, `code_snippet`, `class_name` or `function_name`."#
+        )
+    }
+
+    pub fn user_message_for_generate_search_plan(&self, context: &Context) -> String {
+        format!(
+            r#"<issue>{}</issue>
+<file_context>{}</file_context>
+        "#,
+            context.user_query(),
+            context.file_paths_as_strings().join(", ")
+        )
+    }
+
+    pub fn generate_search_plan(&self, context: Context) -> SearchQuery {
+        println!("googlestudioplangenerator::generate_search_plan");
+
+        println!(
+            "googlestudioplangenerator::generate_search_plan::context: \n{:?}",
+            context
+        );
+
+        let system_message = self.system_message_for_generate_search_plan(&context);
+        let user_message = self.user_message_for_generate_search_plan(&context);
+
+        println!("system_message: {}", system_message);
+        println!("user_message: {}", user_message);
+
+        SearchQuery::new("some query".to_owned())
     }
 }
 

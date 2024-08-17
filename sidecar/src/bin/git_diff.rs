f8036d1a7e861c1b34e5a135a8859bb9e34c0364
skcd <sandeepkumarpani888@gmail.com>
2024-08-17 02:25:19 +0100

    [sidecar] get old and new versions of file from git diff

--- sidecar/src/bin/git_diff.rs
+++ sidecar/src/bin/git_diff.rs
@@ -7,7 +7,7 @@ async fn main() {
     let fs_file_path = "/Users/skcd/scratch/sidecar/sidecar/src/bin/git_diff.rs";
     let mut output = "".to_owned();
     let _ = run_command(&root_directory, fs_file_path, &mut output).await;
-    println!("output:\n{}", &output);
+    // println!("output:\n{}", &output);
 }
 
 use std::fs::File as StdFile;
@@ -50,5 +50,126 @@ async fn run_command(
         output.push('\n');
     }
 
+    // now we parse the git-diff in a very very hacky way, bear with me
+    // example output:
+    println!("git_diff_output\n{}", &output);
+    let results = parse_git_diff_output_full_length(&output);
+    println!("{:?}", results);
+
     Ok(())
+}
+
+#[derive(Debug)]
+struct GitDiffOutput {
+    fs_file_path: String,
+    old_version: String,
+    new_version: String,
+}
+
+// exmaple output:
+// diff --git a/sidecar/src/bin/git_diff.rs b/sidecar/src/bin/git_diff.rs
+// index 1c5d775c..c78ff82f 100644
+// --- a/sidecar/src/bin/git_diff.rs
+// +++ b/sidecar/src/bin/git_diff.rs
+// @@ -50,5 +50,8 @@ async fn run_command(
+//          output.push('\n');
+//      }
+
+// +    // now we parse the git-diff in a very very hacky way, bear with me
+// +    //
+// +
+//      Ok(())
+//  }
+
+fn parse_git_diff_output_full_length(git_diff: &str) -> Vec<GitDiffOutput> {
+    let mut diff_outputs = Vec::new();
+    let git_diff_lines = git_diff.lines().into_iter().collect::<Vec<_>>();
+    println!("git_diff_lines_len({})", git_diff_lines.len());
+    // let sections = git_diff.split("diff --git").skip(1);
+
+    for (mut idx, _git_diff_line) in git_diff_lines.iter().enumerate() {
+        // let lines: Vec<&str> = section.lines().collect();
+        if !is_valid_diff_section(idx, &git_diff_lines) {
+            continue;
+        }
+
+        // now we want to go until we find the next such index
+        let index_limit = (idx + 1..=git_diff_lines.len() - 1)
+            .find(|idx| is_valid_diff_section(*idx, &git_diff_lines));
+        println!("index_limit({:?})::idx({})", index_limit, idx);
+        let section_limit = if index_limit.is_none() {
+            // this implies the end of the section
+            git_diff_lines.len() - 1
+        } else {
+            index_limit.expect("is_none check above to hold") - 1
+        };
+
+        let fs_file_path = extract_file_path(&git_diff_lines[idx]);
+        // we start after the diff git -- and index {crap} lines
+        let slice_limits = &git_diff_lines[idx + 2..=section_limit];
+        let (old_version, new_version) = extract_versions(slice_limits);
+
+        diff_outputs.push(GitDiffOutput {
+            fs_file_path,
+            old_version,
+            new_version,
+        });
+        idx = section_limit;
+    }
+
+    diff_outputs
+}
+
+fn is_valid_diff_section(line_index: usize, git_diff_lines: &Vec<&str>) -> bool {
+    line_index + 1 < git_diff_lines.len()
+        && git_diff_lines[line_index].starts_with("diff --git")
+        && git_diff_lines[line_index + 1].starts_with("index")
+}
+
+fn extract_file_path(line: &str) -> String {
+    println!("extract_file_path::({})", line);
+    let parts: Vec<&str> = line.split_whitespace().collect();
+    if parts.len() >= 3 {
+        if parts[2].starts_with("a/") {
+            parts[2].trim_start_matches("a/").to_owned()
+        } else {
+            parts[2].trim().to_owned()
+        }
+    } else {
+        String::new()
+    }
+}
+
+fn extract_versions(lines: &[&str]) -> (String, String) {
+    let mut old_version = String::new();
+    let mut new_version = String::new();
+    for line in lines {
+        if line.starts_with("@@") {
+            continue;
+        }
+        if line.starts_with("---") {
+            continue;
+        }
+        if line.starts_with("+++") {
+            continue;
+        }
+
+        if line.starts_with('-') {
+            old_version.push_str(&line[1..]);
+            old_version.push('\n');
+        } else if line.starts_with('+') {
+            new_version.push_str(&line[1..]);
+            new_version.push('\n');
+        } else {
+            old_version.push_str(&line[1..]);
+            old_version.push_str("\n");
+            new_version.push_str(&line[1..]);
+            new_version.push_str("\n");
+        }
+    }
+
+    (
+        old_version.trim().to_string(),
+        new_version.trim().to_string(),
+    )
 }

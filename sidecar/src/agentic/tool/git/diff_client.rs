9a27aa170249cda6e53459da4a34aeb52d79531e
skcd <sandeepkumarpani888@gmail.com>
2024-08-19 12:20:35 +0100

    [sidecar] figure out if its anchored editing

--- sidecar/src/agentic/tool/git/diff_client.rs
+++ sidecar/src/agentic/tool/git/diff_client.rs
@@ -75,7 +75,7 @@ async fn run_command(
         .current_dir(root_directory)
         .arg("diff")
         .arg("--no-prefix")
-        .arg("-U7000")
+        .arg("-U8000")
         .stdout(Stdio::from(StdFile::create(&tmpfile_path)?))
         .status()
         .await?;

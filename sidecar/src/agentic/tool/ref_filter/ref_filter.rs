b274d821e294b19253a05331f72e922fa743bcce
sartoshi-foot-dao <99770068+sartoshi-foot-dao@users.noreply.github.com>
2024-08-29 10:44:57 +0100

    [sidecar] add boilerplate

--- sidecar/src/agentic/tool/ref_filter/ref_filter.rs
+++ sidecar/src/agentic/tool/ref_filter/ref_filter.rs
@@ -1,8 +1,50 @@
+use async_trait::async_trait;
+use llm_client::broker::LLMBroker;
 use std::sync::Arc;
 
-use llm_client::broker::LLMBroker;
+use crate::agentic::{
+    symbol::identifier::{LLMProperties, SymbolIdentifier},
+    tool::{errors::ToolError, input::ToolInput, output::ToolOutput, r#type::Tool},
+};
 
-use crate::agentic::symbol::identifier::LLMProperties;
+#[derive(Debug, Clone, serde::Serialize)]
+pub struct ReferenceFilterRequest {
+    user_instruction: String,
+    references: Vec<SymbolIdentifier>, // todo(zi) this needs to be considered.
+    llm_properties: LLMProperties,
+    root_id: String,
+}
+
+impl ReferenceFilterRequest {
+    pub fn new(
+        user_instruction: String,
+        references: Vec<SymbolIdentifier>,
+        llm_properties: LLMProperties,
+        root_id: String,
+    ) -> Self {
+        Self {
+            user_instruction,
+            references,
+            llm_properties,
+            root_id,
+        }
+    }
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct ReferenceFilterResponse {
+    references: Vec<SymbolIdentifier>,
+}
+
+impl ReferenceFilterResponse {
+    pub fn new(references: Vec<SymbolIdentifier>) -> Self {
+        Self { references }
+    }
+
+    pub fn references(&self) -> &[SymbolIdentifier] {
+        &self.references
+    }
+}
 
 pub struct ReferenceFilterBroker {
     llm_client: Arc<LLMBroker>,
@@ -15,5 +57,12 @@ impl ReferenceFilterBroker {
             llm_client,
             fail_over_llm,
         }
+    }
+}
+
+#[async_trait]
+impl Tool for ReferenceFilterBroker {
+    async fn invoke(&self, input: ToolInput) -> Result<ToolOutput, ToolError> {
+        todo!();
     }
 }

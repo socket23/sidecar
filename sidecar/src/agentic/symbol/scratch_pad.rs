1397a51c0b8713e4c78a2251e822e78c1cf6bb2c
skcd <sandeepkumarpani888@gmail.com>
2024-09-10 17:20:56 +0100

    [sidecar] update scratch pad async

--- sidecar/src/agentic/symbol/scratch_pad.rs
+++ sidecar/src/agentic/symbol/scratch_pad.rs
@@ -8,19 +8,24 @@ use std::{pin::Pin, sync::Arc};
 use futures::{stream, Stream, StreamExt};
 use tokio::sync::mpsc::UnboundedSender;
 
-use crate::agentic::symbol::ui_event::UIEventWithID;
+use crate::{
+    agentic::symbol::ui_event::UIEventWithID,
+    chunking::text_document::{Position, Range},
+};
 
 use super::{
     errors::SymbolError,
     events::{
+        edit::{SymbolToEdit, SymbolToEditRequest},
         environment_event::EnvironmentEventType,
         human::{HumanAnchorRequest, HumanMessage},
         message_event::{SymbolEventMessage, SymbolEventMessageProperties},
         types::SymbolEvent,
     },
+    identifier::SymbolIdentifier,
     tool_box::ToolBox,
     tool_properties::ToolProperties,
-    types::SymbolEventRequest,
+    types::{SymbolEventRequest, SymbolEventResponse},
 };
 
 /// Different kind of events which can happen
@@ -28,6 +33,7 @@ use super::{
 
 #[derive(Clone)]
 pub struct ScratchPadAgent {
+    storage_fs_path: String,
     message_properties: SymbolEventMessageProperties,
     tool_box: Arc<ToolBox>,
     symbol_event_sender: UnboundedSender<SymbolEventMessage>,
@@ -40,6 +46,7 @@ impl ScratchPadAgent {
         symbol_event_sender: UnboundedSender<SymbolEventMessage>,
     ) -> Self {
         Self {
+            storage_fs_path: "/Users/skcd/test_repo/sidecar/scratchpad.md".to_owned(),
             message_properties,
             tool_box,
             symbol_event_sender,
@@ -102,7 +109,13 @@ impl ScratchPadAgent {
             )
             .await?;
 
-        let _edits_done = stream::iter(symbols_to_edit_request.into_iter().map(|data| {
+        let cloned_self = self.clone();
+        let cloned_user_query = anchor_request.user_query().to_owned();
+        let _ = tokio::spawn(async move {
+            cloned_self.mark_start_of_iteration(cloned_user_query).await;
+        });
+
+        let edits_done = stream::iter(symbols_to_edit_request.into_iter().map(|data| {
             (
                 data,
                 self.message_properties.clone(),
@@ -129,7 +142,18 @@ impl ScratchPadAgent {
         // run 100 edit requests in parallel to prevent race conditions
         .buffer_unordered(100)
         .collect::<Vec<_>>()
-        .await;
+        .await
+        .into_iter()
+        .filter_map(|s| s.ok())
+        .collect::<Vec<_>>();
+
+        let cloned_self = self.clone();
+        let cloned_user_query = anchor_request.user_query().to_owned();
+        let _ = tokio::spawn(async move {
+            let _ = cloned_self
+                .react_to_edits(edits_done, cloned_user_query)
+                .await;
+        });
         println!(
             "scratch_pad_agent::human_message_anchor::end::time_taken({}ms)",
             start_instant.elapsed().as_millis()
@@ -142,5 +166,83 @@ impl ScratchPadAgent {
                 self.message_properties.request_id_str().to_owned(),
             ));
         Ok(())
+    }
+
+    async fn mark_start_of_iteration(&self, user_query: String) {
+        println!("scratch_pad::mark_start_of_iteration");
+        let scratch_pad = self.storage_fs_path.to_owned();
+        let symbols_to_edit_request = SymbolToEditRequest::new(vec![SymbolToEdit::new(
+            scratch_pad.to_owned(),
+            Range::new(Position::new(0, 0, 0), Position::new(0, 0, 0)),
+            scratch_pad.to_owned(),
+            vec![format!(r#"Record your start on the following user query:
+{user_query}"#)],
+            false,
+            false,
+            true,
+            "Your job is to track the user query in your scratch pad, this way you are able to keep track of the work you are going to do".to_owned(),
+            None,
+            false,
+            None,
+            false,
+        )], SymbolIdentifier::with_file_path(&scratch_pad, &scratch_pad), vec![]);
+        let (sender, _) = tokio::sync::oneshot::channel();
+        let symbol_event_request = SymbolEventRequest::new(
+            symbols_to_edit_request.symbol_identifier().clone(),
+            SymbolEvent::Edit(symbols_to_edit_request),
+            ToolProperties::new(),
+        );
+        let event = SymbolEventMessage::message_with_properties(
+            symbol_event_request,
+            self.message_properties.clone(),
+            sender,
+        );
+        let _ = self.symbol_event_sender.send(event);
+    }
+
+    /// We want to react to the various edits which have happened and the request they were linked to
+    /// and come up with next steps and try to understand what we can do to help the developer
+    async fn react_to_edits(&self, edits: Vec<SymbolEventResponse>, user_query: String) {
+        println!("scratch_pad::react_to_edits");
+        let after_edits_changes = edits
+            .into_iter()
+            .map(|symbol_event_response| symbol_event_response.to_string())
+            .collect::<Vec<_>>()
+            .join("\n");
+
+        let scratch_pad = self.storage_fs_path.to_owned();
+
+        let symbols_to_edit_request = SymbolToEditRequest::new(vec![SymbolToEdit::new(
+                    scratch_pad.to_owned(),
+                    Range::new(Position::new(0, 0, 0), Position::new(0, 0, 0)),
+                    scratch_pad.to_owned(),
+                    vec![format!(r#"Record your insights from working on the user query here, use this as a running notepad:
+<user_query>
+{user_query}
+</user_query>
+<changes_made>
+{after_edits_changes}
+</changes_made>"#).to_owned()],
+                    false,
+                    false,
+                    true,
+                    "Record your insights from working on the user query here, use this as a running notepad".to_owned(),
+                    None,
+                    false,
+                    None,
+                    true,
+                )], SymbolIdentifier::with_file_path(&scratch_pad, &scratch_pad), vec![]);
+        let (sender, _) = tokio::sync::oneshot::channel();
+        let symbol_event_request = SymbolEventRequest::new(
+            symbols_to_edit_request.symbol_identifier().clone(),
+            SymbolEvent::Edit(symbols_to_edit_request),
+            ToolProperties::new(),
+        );
+        let event = SymbolEventMessage::message_with_properties(
+            symbol_event_request,
+            self.message_properties.clone(),
+            sender,
+        );
+        let _ = self.symbol_event_sender.send(event);
     }
 }

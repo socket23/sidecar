262e24a29c1a1e0b5c9e56278a800d7628815498
sartoshi-foot-dao <99770068+sartoshi-foot-dao@users.noreply.github.com>
2024-09-24 15:36:09 +0100

    [sidecar] make binary

---
+++ sidecar/src/bin/plan_step_edit.rs
@@ -0,0 +1,123 @@
+use std::{path::PathBuf, sync::Arc};
+
+use llm_client::{
+    broker::LLMBroker,
+    clients::types::LLMType,
+    config::LLMBrokerConfiguration,
+    provider::{
+        AnthropicAPIKey, FireworksAPIKey, GoogleAIStudioKey, LLMProvider, LLMProviderAPIKeys,
+        OpenAIProvider,
+    },
+};
+use sidecar::{
+    agentic::{
+        symbol::{
+            events::{
+                input::{SymbolEventRequestId, SymbolInputEvent},
+                message_event::SymbolEventMessageProperties,
+            },
+            identifier::LLMProperties,
+            manager::SymbolManager,
+        },
+        tool::{
+            broker::{ToolBroker, ToolBrokerConfiguration},
+            code_edit::models::broker::CodeEditBroker,
+        },
+    },
+    chunking::{editor_parsing::EditorParsing, languages::TSLanguageParsing},
+    inline_completion::symbols_tracker::SymbolTrackerInline,
+    user_context::types::UserContext,
+};
+
+fn default_index_dir() -> PathBuf {
+    match directories::ProjectDirs::from("ai", "codestory", "sidecar") {
+        Some(dirs) => dirs.data_dir().to_owned(),
+        None => "codestory_sidecar".into(),
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let request_id = uuid::Uuid::new_v4();
+    let request_id_str = request_id.to_string();
+    let parea_url = format!(
+        r#"https://app.parea.ai/logs?colViz=%7B%220%22%3Afalse%2C%221%22%3Afalse%2C%222%22%3Afalse%2C%223%22%3Afalse%2C%22error%22%3Afalse%2C%22deployment_id%22%3Afalse%2C%22feedback_score%22%3Afalse%2C%22time_to_first_token%22%3Afalse%2C%22scores%22%3Afalse%2C%22start_timestamp%22%3Afalse%2C%22user%22%3Afalse%2C%22session_id%22%3Afalse%2C%22target%22%3Afalse%2C%22experiment_uuid%22%3Afalse%2C%22dataset_references%22%3Afalse%2C%22in_dataset%22%3Afalse%2C%22event_type%22%3Afalse%2C%22request_type%22%3Afalse%2C%22evaluation_metric_names%22%3Afalse%2C%22request%22%3Afalse%2C%22calling_node%22%3Afalse%2C%22edges%22%3Afalse%2C%22metadata_evaluation_metric_names%22%3Afalse%2C%22metadata_event_type%22%3Afalse%2C%22metadata_0%22%3Afalse%2C%22metadata_calling_node%22%3Afalse%2C%22metadata_edges%22%3Afalse%2C%22metadata_root_id%22%3Afalse%7D&filter=%7B%22filter_field%22%3A%22meta_data%22%2C%22filter_operator%22%3A%22equals%22%2C%22filter_key%22%3A%22root_id%22%2C%22filter_value%22%3A%22{request_id_str}%22%7D&page=1&page_size=50&time_filter=1m"#
+    );
+    println!("===========================================\nRequest ID: {}\nParea AI: {}\n===========================================", request_id.to_string(), parea_url);
+    let editor_url = "http://localhost:42425".to_owned();
+    let anthropic_api_keys = LLMProviderAPIKeys::Anthropic(AnthropicAPIKey::new("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".to_owned()));
+    let anthropic_llm_properties = LLMProperties::new(
+        LLMType::ClaudeSonnet,
+        LLMProvider::Anthropic,
+        anthropic_api_keys.clone(),
+    );
+    let editor_parsing = Arc::new(EditorParsing::default());
+    let symbol_broker = Arc::new(SymbolTrackerInline::new(editor_parsing.clone()));
+    let tool_broker = Arc::new(ToolBroker::new(
+        Arc::new(
+            LLMBroker::new(LLMBrokerConfiguration::new(default_index_dir()))
+                .await
+                .expect("to initialize properly"),
+        ),
+        Arc::new(CodeEditBroker::new()),
+        symbol_broker.clone(),
+        Arc::new(TSLanguageParsing::init()),
+        // for our testing workflow we want to apply the edits directly
+        ToolBrokerConfiguration::new(None, true),
+        LLMProperties::new(
+            LLMType::Gpt4O,
+            LLMProvider::OpenAI,
+            LLMProviderAPIKeys::OpenAI(OpenAIProvider::new(
+                "sk-proj-BLaSMsWvoO6FyNwo9syqT3BlbkFJo3yqCyKAxWXLm4AvePtt".to_owned(),
+            )),
+        ),
+    ));
+
+    let user_context = UserContext::new(vec![], vec![], None, vec![]);
+
+    let (sender, mut _receiver) = tokio::sync::mpsc::unbounded_channel();
+
+    let _event_properties = SymbolEventMessageProperties::new(
+        SymbolEventRequestId::new("".to_owned(), "".to_owned()),
+        sender.clone(),
+        editor_url.to_owned(),
+        tokio_util::sync::CancellationToken::new(),
+    );
+
+    let _symbol_manager = SymbolManager::new(
+        tool_broker.clone(),
+        symbol_broker.clone(),
+        editor_parsing,
+        anthropic_llm_properties.clone(),
+    );
+
+    let problem_statement = "add a new field user_id to the Tag struct".to_owned();
+
+    let root_dir = "/Users/zi/codestory/sidecar/sidecar/src";
+
+    let _initial_request = SymbolInputEvent::new(
+        user_context,
+        LLMType::ClaudeSonnet,
+        LLMProvider::Anthropic,
+        anthropic_api_keys,
+        problem_statement,
+        request_id.to_string(),
+        request_id.to_string(),
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        true, // full code editing
+        Some(root_dir.to_string()),
+        None,
+        true, // big_search
+        sender,
+    );
+
+    // so we just want to use search and replace, right?
+
+    // not even, we just want to invoke the tool.
+}

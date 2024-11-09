8e561ead6869fdd2fffc9c7fd4080d5aa7b7e5cf
skcd <sandeepkumarpani888@gmail.com>
2024-11-09 03:35:02 +0000

    [sidecar] cleanup upload paths

--- package.sh
+++ package.sh
@@ -8,4 +8,4 @@
 # We can use this command to zip the whole thing together for the platform we are
 # interetsed in, right now we will lock it to just mac
 cargo build --bin webserver --release
-zip -r sidecar sidecar/onnxruntime/ sidecar/qdrant/ target/release/webserver sidecar/models/
+zip -r sidecar target/release/webserver

51c72bb992ace5e081f4acdda259b33ada9aa8b6
skcd <sandeepkumarpani888@gmail.com>
2024-11-19 17:11:20 +0000

    [sidecar] improve prompt

--- sidecar/src/agentic/tool/session/tool_use_agent.rs
+++ sidecar/src/agentic/tool/session/tool_use_agent.rs
@@ -96,9 +96,10 @@ impl ToolUseAgent {
         let operating_system = self.operating_system.to_owned();
         let default_shell = self.shell.to_owned();
         format!(
-            r#"You are an expert software engineer tasked with solving Github issues which the user will provide. You are an expert at {repo_name} and will use the tools available below to the best of your abilities to help solve the user.
+            r#"You are an expert software engineer tasked with solving Github issues which the user will provide. You are an expert at {repo_name} and you will be given a list of tools which you can use one after the other to debug and fix the issue.
 The user is pretty sure that all the information to solve the issue is present within the {working_directory} which they have cloned for to work on the issue.
 If you are going to execute any python commands or scripts you should always use `python3` and nothing else.
+The end goal is to fix the issue in the current {working_directory}. You have to make sure that the bug is fixed at the end when you are done with your changes.
 Do your very best, you got this!
 ====
 
@@ -211,7 +212,8 @@ You accomplish a given task iteratively, breaking it down into clear steps and w
 1. Analyze the user's task and set clear, achievable goals to accomplish it. Prioritize these goals in a logical order.
 2. Work through these goals sequentially, utilizing available tools one at a time as necessary. Each goal should correspond to a distinct step in your problem-solving process. You will be informed on the work completed and what's remaining as you go.
 3. Remember, you have extensive capabilities with access to a wide range of tools that can be used in powerful and clever ways as necessary to accomplish each goal. Before calling a tool, do some analysis within <thinking></thinking> tags. First, analyze the file structure provided in environment_details to gain context and insights for proceeding effectively. Then, think about which of the provided tools is the most relevant tool to accomplish the user's task. Next, go through each of the required parameters of the relevant tool and determine if the user has directly provided or given enough information to infer a value. When deciding if the parameter can be inferred, carefully consider all the context to see if it supports a specific value. If all of the required parameters are present or can be reasonably inferred, close the thinking tag and proceed with the tool use. BUT, if one of the values for a required parameter is missing, DO NOT invoke the tool (not even with fillers for the missing params) and instead, ask the user to provide the missing parameters using the ask_followup_question tool. DO NOT ask for more information on optional parameters if it is not provided.
-4. Once you've completed the user's task, you must use the attempt_completion tool to present the result of the task to the user. You may also provide a CLI command to showcase the result of your task; this can be particularly useful for web development tasks, where you can run e.g. \`open index.html\` to show the website you've built."#
+4. Once you've completed the user's task, you must use the attempt_completion tool to present the result of the task to the user.
+5. You can ONLY USE 1 TOOL in each step and not multiple tools, using multiple tools is not allowed."#
         )
     }
 
